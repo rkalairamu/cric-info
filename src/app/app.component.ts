05ce4fd306e2222004a64a7a62ec9db77f55275a
Kalaiselvan Ramu <kramu@apttus.com>
2018-11-04 15:50:41 +0530

    initial project setup & adding necessary npm modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {User} from './service/user';
+import {UserService} from './service/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Cric Info';
+  user: any;
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit() {
+    this.user = this.userService.getLoginUser();
+  }
+
+  /**
+   * group_id 1 - admin & 2 - normal user
+   * @param {User} user
+   */
+  checkUser(user: User) {
+    this.user = this.userService.setLoginUser(user);
+  }
+
+  logout() {
+    this.user = this.userService.logout();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
+import {PlayerModule} from './player/player.module';
+import {LoginModule} from './login/login.module';
+
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    LoginModule,
+    PlayerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
